36ed9fc76237f7b0e089029a3c822327df090e3c
K1o0b1a9 <your.email@example.com>
2020-11-11 06:48:29 +0000

    Delete contact routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   root  "home#top"
   get "about" => "home#about"
   get "index" => "posts#index"
-<<<<<<< HEAD
-  get "contact" => "contacts#new"
-=======
   get "contact" => "home#contact"
->>>>>>> add-contactform
   
 end
