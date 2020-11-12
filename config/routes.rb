a3b6d5314f29b5725673bf5551ae99e25d3c00c0
K1o0b1a9 <your.email@example.com>
2020-11-12 05:17:01 +0000

    Remove adminpage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :admin_accounts, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
   root  "home#top"
   get "about" => "home#about"
   get "index" => "posts#index"
