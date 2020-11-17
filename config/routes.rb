69dc932183b348e1298cf604e13ad8ec76292211
K1o0b1a9 <your.email@example.com>
2020-11-17 08:46:42 +0000

    Modify tweets page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root  "home#top"
   get "about" => "home#about"
   #投稿画面
-  get "tweet/new" => "posts#new"
-  post "tweet" => "posts#create"
+  get "tweet" => "posts#new"
+  post "tweet/success" => "posts#create"
   #問い合わせフォーム
   get "contact" => "home#contact"
   #下記問い合わせフォームはSendgrid使えないから保留
