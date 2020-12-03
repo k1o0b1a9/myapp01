d6cdaaab7173c6581baad750ebb595524af2687a
K1o0b1a9 <your.email@example.com>
2020-12-03 04:29:21 +0000

    fix Edit data process

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,7 @@ class PostsController < ApplicationController
   
   def update
     @post=Post.find_by(id: params[:id])
+    @post.title=params[:title]
     @post.content=params[:content]
     @post.save
     redirect_to(root_path)
